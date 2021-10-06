fb33dd60e7821ed3b9d7931a301a7b55b26a2b37
Abdullaev Farhad <abdullaev@prg-it.ru>
2021-10-06 20:39:47 +0500

    Добавил Лейзи-лоадинг.

--- pet-finder/src/app/app-routing.module.ts
+++ pet-finder/src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainPageModule} from "./pages/main-page/main-page.module";
+import {AuthorisationPageModule} from "./pages/authorisation-page/authorisation-page.module";
 
 const routes: Routes = [
-  { path: '', component: MainPageModule },
-  { pathMatch: '/auth', loadChildren: () => import("./pages/authorisation-page/authorisation-page.module")}
-
+  { path: 'auth', loadChildren: () => import("./pages/authorisation-page/authorisation-page.module").then((module) => module.AuthorisationPageModule)},
+  { path: '', loadChildren: ()=> import("./pages/main-page/main-page.module").then((module)=> module.MainPageModule)}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), MainPageModule, AuthorisationPageModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
